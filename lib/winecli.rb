d9549fd81acfad6ca5fb7c73e563a3cfa8a4039d
Kelsey Fecho <kfecho30@gmail.com>
2018-03-27 09:38:34 -0400

    Added WineCLIController to required files

--- lib/winecli.rb
+++ lib/winecli.rb
@@ -2,6 +2,7 @@ require "winecli/version"
 require "winecli/Scraper"
 require "winecli/WineList"
 require "winecli/Wine"
+require "winecli/WineCLIController.rb"
 
 module Winecli
   # Your code goes here...
